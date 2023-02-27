776eb13be4b5692786c0d0d7bf72f473e0f3d16f
prisam1 <pritamsamaddar840@gmail.com>
2023-02-27 20:57:17 +0530

    done

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true, 
+  secret: 'SECRET' 
+}));
+
+app.get('/', function(req, res) {
+  res.render('pages/auth');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+
+
+const passport = require('passport');
+var userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '116899161955996831498';
+const GOOGLE_CLIENT_SECRET = '367be4b3891581af7c111251f2af0f11b3d15763';
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+   
+    res.redirect('/success');
+  });
